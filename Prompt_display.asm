5d923c3146119100e59ddff3e098e33d64c8e689
ephraimnix <bonanmarcephraim@gmail.com>
2025-02-09 02:10:41 +0100

    Update Prompt_display.asm

--- Prompt_display.asm
+++ Prompt_display.asm
@@ -13,4 +13,29 @@ _start :
 ; Premièrement on doit afficher le langage
 ; Ensuite on peut lire l'entrée de l'utilisateur
 ; Puis on l'affiche avant de finir l'éxécution du programme
-  
+
+    mov rax, 1                                
+    mov rdi, 1                                
+    mov rsi, prompt                            
+    mov rdx, prompt_len                        
+    syscall
+
+; Lecture de l'entrée
+
+    mov rax, 0                               
+    mov rdi, 0                                
+    mov rsi, input                           
+    mov rdx, 128                             
+    syscall
+
+; Affichage de l'entrée 
+    mov rax, 1                                
+    mov rdi, 1                               
+    mov rsi, input                           
+    mov rdx, rax                             
+    syscall
+
+; Fin du programme
+    mov rax, 60                            
+    xor rdi, rdi                              
+    syscall
