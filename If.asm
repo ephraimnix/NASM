43a908916ccb578c33b1295bea95dc1746fb3dbd
ephraimnix <bonanmarcephraim@gmail.com>
2025-02-10 00:08:05 +0100

    Update If.asm

--- If.asm
+++ If.asm
@@ -5,7 +5,25 @@ section.bss
    var resb 1
 
 section.text
-
-global_start
-
+  global_start
+  
 _start :
+  boucle: 
+  move byte [var], 0
+  move al, [var],
+  cmp al, 5
+  je end
+  inc byte [var]
+  jmp boucle
+
+end : 
+  move eax, 4
+  move ebx, 1
+  move ecx, txt
+  move edx, 18
+
+   move eax, 1
+   xor ebx, ebx
+   int 0x80
+
+
